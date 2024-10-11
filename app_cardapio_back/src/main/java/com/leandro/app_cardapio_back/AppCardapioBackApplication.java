41c688a1b1883bc20ed0739e5944671ec4e8f5b6
leandro-silva <leandro.tecnicoinfo@gmail.com>
2024-10-11 19:18:58 -0300

    segundo commit

---
+++ app_cardapio_back/src/main/java/com/leandro/app_cardapio_back/AppCardapioBackApplication.java
@@ -0,0 +1,13 @@
+package com.leandro.app_cardapio_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCardapioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCardapioBackApplication.class, args);
+	}
+
+}
---
+++ app_cardapio_back/src/test/java/com/leandro/app_cardapio_back/AppCardapioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.app_cardapio_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCardapioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
